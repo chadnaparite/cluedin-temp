979552b620010199c97d57240a915ff081c7334a
Richard Naparite <chad.naparite@gmail.com>
2022-01-05 16:26:39 +0800

    RelatedEntities2

---
+++ src/RelatedEntities2/BaseRelatedEntitiesProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.DataStore.Document.Models;
+using CluedIn.RelatedEntities;
+using Microsoft.Extensions.Logging;
+
+namespace CluedIn.RelatedEntities2
+{
+    public abstract class BaseRelatedEntitiesProvider : IRelatedEntitiesProvider
+    {
+        public readonly EntityType EntityType;
+        public readonly string OriginName = null;
+
+        public BaseRelatedEntitiesProvider(EntityType entityType)
+        {
+            EntityType = entityType;
+        }
+        public BaseRelatedEntitiesProvider(EntityType entityType, string originName) : this(entityType)
+        {
+            OriginName = originName;
+        }
+
+        public abstract IEnumerable<SuggestedSearch> SuggestedSearches(Guid id);
+        public List<SuggestedSearch> searches = new List<SuggestedSearch>();
+        public RelatedEntitiesHelper relatedEntitiesHelper { get; set; }
+
+        public IEnumerable<SuggestedSearch> GetRelatedEntitiesSearches(ExecutionContext context, Entity entity)
+        {
+            if (entity.Type == EntityType && (entity.OriginEntityCode.Origin == OriginName || OriginName == null))
+            {
+                var Log = context.Log;
+                Log.LogInformation($"[Related Entities] {EntityType}{", " + OriginName}");
+
+                relatedEntitiesHelper = new RelatedEntitiesHelper(context, entity);
+
+                foreach (var suggestedSearch in SuggestedSearches(entity.Id))
+                {
+                    try
+                    {
+                        if (RelatedEntitiesUtility.CypherFluentQueriesCount(suggestedSearch.SearchQuery, suggestedSearch.Tokens, context) > 0)
+                            searches.Add(suggestedSearch);
+                        else
+                            Log.LogInformation($"[Related Entities] No result. Query: '{suggestedSearch.SearchQuery}' Token: '{suggestedSearch.Tokens}'");
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.LogCritical(ex, $"[Related Entities] Error in executing Suggested Search. Query: '{suggestedSearch.SearchQuery}' Token: '{suggestedSearch.Tokens}'");
+                    }
+                }
+
+                return searches;
+            }
+
+            return new SuggestedSearch[0];
+        }
+    }
+}
---
+++ src/RelatedEntities2/RelatedEntitesHelper.cs
@@ -0,0 +1,89 @@
+﻿using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.DataStore.Document.Models;
+
+namespace CluedIn.RelatedEntities2
+{
+    public class RelatedEntitiesHelper
+    {
+        private readonly ExecutionContext context;
+        private readonly Entity entity;
+
+        public RelatedEntitiesHelper(ExecutionContext _context, Entity _entity)
+        {
+            context = _context;
+            entity = _entity;
+        }
+
+        public SuggestedSearch GetRelationshipForEntity(string displayName, EntityEdgeType edgeType)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}}",
+                Tokens = string.Format("{0},{1}", edgeType, entity.Id.ToString()),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOrderByField(string displayName, EntityEdgeType edgeType, string orderField)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} order by {{FIELD}}",
+                Tokens = string.Format("{0},{1},{2}", edgeType, entity.Id.ToString(), orderField),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOfMultipleTypes(string displayName, EntityEdgeType edgeType, params EntityType[] entityTypes)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} of types {{TYPE}}",
+                Tokens = string.Format("{0},{1},{2}", edgeType, entity.Id.ToString(), string.Join(",", entityTypes.ToString())),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOfType(string displayName, EntityEdgeType edgeType, EntityType entityType)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} of type {{TYPE}}",
+                Tokens = string.Format("{0},{1},{2}", edgeType, entity.Id.ToString(), entityType),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOfTypeOrderByField(string displayName, EntityEdgeType edgeType, EntityType entityType, string orderField)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} of type {{TYPE}} order by {{FIELD}}",
+                Tokens = string.Format("{0},{1},{2},{3}", edgeType, entity.Id.ToString(), entityType, orderField),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOfTypeToEntityOfType(string displayName, EntityEdgeType edgeType, EntityType entityFrom, EntityType entityTo)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} of type {{TYPE}}->{{TYPE2}}",
+                Tokens = string.Format("{0},{1},{2}", edgeType, entity.Id.ToString(), entityFrom, entityTo),
+                Type = "List"
+            };
+        }
+        public SuggestedSearch GetRelationshipForEntityOfTypeFromEntityOfType(string displayName, EntityEdgeType edgeType, EntityType entityFrom, EntityType entityTo)
+        {
+            return new SuggestedSearch
+            {
+                DisplayName = displayName,
+                SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}} of type {{TYPE}}<-{{TYPE2}}",
+                Tokens = string.Format("{0},{1},{2}", edgeType, entity.Id.ToString(), entityFrom, entityTo),
+                Type = "List"
+            };
+        }
+    }
+}
